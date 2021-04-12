fb0379780a60c8515b5c69ae9c964b7fba03802c
binslapateste <felipems172019@gmail.com>
2021-04-12 18:11:26 -0300

    First commit

---
+++ src/help1.js
@@ -0,0 +1,38 @@
+const help1 = (prefix) => {
+
+	return `
+╭────────────────╮
+ *COMANDOS DO BINSLIL*
+╰────────────────╯
+ 
+➤ *${prefix}marcar*
+➤ *${prefix}marcar2*
+➤ *${prefix}marcar3*
+➤ *${prefix}loli*
+➤ *${prefix}loli1*
+➤ *${prefix}hentai*
+➤ *${prefix}dono*
+➤ *${prefix}porno*
+➤ *${prefix}boanoite*
+➤ *${prefix}bomdia*
+➤ *${prefix}boatarde*
+➤ *${prefix}mia*
+➤ *${prefix}mia1*
+➤ *${prefix}mia2*
+➤ *${prefix}belle*
+➤ *${prefix}belle1*
+➤ *${prefix}belle2*
+➤ *${prefix}belle3*
+➤ *${prefix}akeno*
+➤ *${prefix}meme*
+➤ *${prefix}lofi*
+➤ *${prefix}malkova*
+➤ *${prefix}canal*
+➤ *${prefix}nsfwloli*
+➤ *${prefix}reislin*
+➤ *${prefix}limpar*
+➤ *${prefix}marcar*
+➤ *${prefix}ts (texto que deseja transmitir)*
+}
+exports.help1 = help1
+
